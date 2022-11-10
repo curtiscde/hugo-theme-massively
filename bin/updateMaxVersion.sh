63dc7123fcc659d48fa21ced947ff204a19e7095
curtiscde <curt@live.co.uk>
2022-11-10 08:14:53 +0000

    ⬆️ Upgrade Hugo: v0.105.0

--- bin/updateMaxVersion.sh
+++ bin/updateMaxVersion.sh
@@ -8,6 +8,6 @@ git add -p
 
 git commit -m "⬆️ Upgrade Hugo: v$hugoversion"
 
-git push --set-upstream origin hugo-$verhugoversionsion
+git push --set-upstream origin hugo-v$hugoversion
 
 gh pr create -d --title "⬆️ Upgrade Hugo: v$hugoversion" --body "Upgrades the max compatible version of Hugo to [v$hugoversion](https://github.com/gohugoio/hugo/releases/tag/v$hugoversion)"
